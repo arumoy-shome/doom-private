7e8f4d9feb49b39013142603a223e37d1028bec8
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-10-03 16:24:22 +0200

    init: revert back to old doom window nav keybindings

--- config.el
+++ config.el
@@ -21,3 +21,15 @@
 
 ;; editing
 (add-hook! text-mode 'auto-fill-mode)
+
+;; keybindings
+(map!
+ :n "C-h"   #'evil-window-left
+ :n "C-j"   #'evil-window-down
+ :n "C-k"   #'evil-window-up
+ :n "C-l"   #'evil-window-right
+
+ (:after treemacs-evil
+   (:map evil-treemacs-state-map
+     "C-h" #'evil-window-left
+     "C-l" #'evil-window-right)))
